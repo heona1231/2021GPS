3f370512f78adc4b9ed8d87968a7b4d7f2b51eed
DaLae37 <dalae37@gamil.com>
2021-04-02 17:14:24 +0900

    20404서현아

---
+++ Dodge_Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int directionVector = 1;
+    private int speed = 7;
+
+    public void SetDirectionVector(int directionVector)
+    {
+        this.directionVector = directionVector;
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += directionVector * speed * Time.deltaTime;
+        transform.position = pos;
+    
+        if(pos.x < -10 || pos.x>10)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag.Equals("Player"))
+        {
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Dodge_Game/Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int hp = 3;
+
+    //Getter
+    public int GetHp()
+    {
+        return hp;
+    }
+
+    //Setter
+    public void SetHp(int hp)
+    {
+        this.hp = hp;
+    }
+
+    public void Update()
+    {
+        if (hp > 0)
+        {
+            float posX = 0.0f, posY = 0.0f;
+            /*        
+                    if(Input.GetKey(KeyCode.A))
+                    {
+                        posX -= 1;
+                    }
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        posX += 1;
+                    }
+                    if (Input.GetKey(KeyCode.W))
+                    {
+                        posY -= 1;
+                    }
+                    if (Input.GetKey(KeyCode.S))
+                    {
+                        posY += 1;
+                    }   
+             */
+            posX = Input.GetAxis("Horizontal");
+            posY = Input.GetAxis("Vertical");
+
+            Vector3 pos = gameObject.transform.position;
+            pos.x += posX * Time.deltaTime * 10;
+            pos.y += posY * Time.deltaTime * 10;
+
+            if(pos.x < -8.25f)
+            {
+                pos.x = -8.25f;
+            }
+            else if (pos.x > 8.25f)
+            {
+                pos.x = 8.25f;
+            }
+            else if (pos.y < -4.7f)
+            {
+                pos.y = -4.7f;
+            }
+            else if (pos.y > 4.7f)
+            {
+                pos.y = 4.7f;
+            }
+
+            gameObject.transform.position = pos;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag.Equals("Enemy"))
+        {
+            hp -= 1;
+            print(hp);
+        }
+    }
+}
