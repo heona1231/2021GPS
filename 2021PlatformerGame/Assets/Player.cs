ed1e007d6cc4e1c41322d3168b63aa0e32893cee
heona1231 <heona1231@gmail.com>
2021-04-05 17:53:12 +0900

    0405

---
+++ 2021PlatformerGame/Assets/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float moveSpeed = 5.0f;
+    private float jumpPower = 5.0f;
+
+    public Transform groundChecker;
+    public float groundRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rigid;
+    bool isGround = true;
+
+    private Animator anim;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            anim.SetBool("isAttack", true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            anim.SetBool("isAttack", false);
+        }
+        isGround = Physics2D.OverlapCircle(groundChecker.position, groundRadius, groundLayer);
+        Jump();
+        Move();
+    }
+
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && isGround)
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            isGround = false;
+        }
+    }
+
+    void Move()
+    {
+        float posX = Input.GetAxis("Horizontal");
+
+        if (posX != 0)
+        {
+            if(posX >= 0)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+        }
+        transform.Translate(Mathf.Abs(posX) * Vector3.right * moveSpeed * Time.deltaTime);
+    }
+
+}
