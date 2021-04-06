61bc84ab4f0c8362132c9983e9cd3c45809e19d2
heona1231 <heoan1231@gmail.com>
2021-04-06 17:41:06 +0900

    0406

---
+++ 2021PlatformerGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Animator anim;
+    bool isDie = false;
+    float dieTimer = 0;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (isDie)
+        {
+            dieTimer += Time.deltaTime;
+            if (dieTimer >= 0.5f)
+            {
+                SoundManager.instance.HitSound();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void Die()
+    {
+        anim.SetBool("isDie", true);
+        isDie = true;
+    }
+}
---
+++ 2021PlatformerGame/Assets/Scripts/Player.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float moveSpeed = 5.0f;
+    private float jumpPower = 5.0f;
+
+    public Transform groundChecker;
+    public float groundRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rigid;
+    bool isGround = true;
+
+    private Animator anim;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        Jump();
+        Move();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            anim.SetBool("isAttack", true);
+            SoundManager.instance.AtttackSound();
+
+            Collider2D col = Physics2D.OverlapCircle(transform.position, 2, (1 << LayerMask.NameToLayer("Enemy")));
+
+            /*
+            Collider2D[] cols;
+            for(int i = 0; i < cols.Length; i++)
+            {
+
+            }
+            */
+            if (col != null)
+            {
+                col.GetComponent<Enemy>().Die();
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            anim.SetBool("isAttack", false);
+        }
+        isGround = Physics2D.OverlapCircle(groundChecker.position, groundRadius, groundLayer);
+        
+    }
+
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && isGround)
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            isGround = false;
+        }
+    }
+
+    void Move()
+    {
+        float posX = Input.GetAxis("Horizontal");
+
+        if (posX != 0)
+        {
+            if(posX >= 0)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+        }
+        transform.Translate(Mathf.Abs(posX) * Vector3.right * moveSpeed * Time.deltaTime);
+    }
+
+}
